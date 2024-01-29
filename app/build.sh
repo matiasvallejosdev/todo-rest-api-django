436c47e7f1177777fa0873226528c02faa063202
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-01-29 19:49:27 -0300

    Create super userd

--- app/build.sh
+++ app/build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # How to automate createsuperuser on django
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@todo.com').delete(); User.objects.create_user(email='matiasvallejosdev@outlook.com', password='19573')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='matiasvallejosdev@outlook.com').delete(); User.objects.create_superuser(email='matiasvallejosdev@outlook.com', password='19573')"
 
 python manage.py collectstatic --no-input
 python manage.py migrate
