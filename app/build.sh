b70a317c7da328edebce3a303a3e5768b5ed36a9
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-01-29 19:42:13 -0300

    EOF build.sh fixed

--- app/build.sh
+++ app/build.sh
@@ -5,8 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 # How to automate createsuperuser on django
-# https://stackoverflow.com/questions/6244382/how-to-automate-createsuperuser-on-django/59467533#59467533
-python manage.py shell -c "from django.contrib.auth import get_user_model; get_user_model().objects.create_user(email="matiasvallejosdev@outlook.com', password='19573')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@todo.com').delete(); User.objects.create_user(email='matiasvallejosdev@outlook.com', password='19573')"
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 
