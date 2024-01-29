f646852001b6d4e60c69e70b184eb8cd735870ea
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-01-29 18:55:53 -0300

    Edit build.sh

--- app/build.sh
+++ app/build.sh
@@ -7,8 +7,8 @@ pip install -r requirements.txt
 # How to automate createsuperuser on django
 # https://stackoverflow.com/questions/6244382/how-to-automate-createsuperuser-on-django/59467533#59467533
 # python manage.py shell -c "from django.contrib.auth import get_user_model; get_user_model().objects.create_user('game@planetchallenge.com', 'game1986')"
-python app/manage.py collectstatic --no-input
-python app/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
 
-python app/manage.py drop_test_database --noinput
-python app/manage.py test
+python manage.py drop_test_database --noinput
+python manage.py test
