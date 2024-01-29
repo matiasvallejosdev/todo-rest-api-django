ddf8d8773154d90c4d9386d66e4a5f78307099d5
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-01-29 18:25:54 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python app/manage.py collectstatic --no-input
+python app/manage.py migrate
