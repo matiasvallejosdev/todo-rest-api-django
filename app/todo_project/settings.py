3c14e43db31fbd81d8d7321e087e56848149b268
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-01-30 11:49:19 -0300

    Improperly SECRET_KEY named fixed

--- app/todo_project/settings.py
+++ app/todo_project/settings.py
@@ -19,7 +19,7 @@ dotenv.load_dotenv(dotenv_path)
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJ_SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
