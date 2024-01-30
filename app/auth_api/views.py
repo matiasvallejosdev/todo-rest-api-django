ca9b5f1aafee95f31bb4995145efc50da452a955
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-01-30 13:50:13 -0300

    Fix views google auth

--- app/auth_api/views.py
+++ app/auth_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework import permissions
 
+import os
+
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
@@ -10,7 +12,7 @@ from rest_framework.response import Response
 
 class GoogleLoginView(SocialLoginView):  # Authorization Code grant
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://localhost:3000/api/auth/callback/google"
+    callback_url = f"{os.environ.get("BASE_URL", 'http://localhost:3000')}/api/auth/callback/google"
     client_class = OAuth2Client
 
 
