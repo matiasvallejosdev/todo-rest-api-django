146b535355ec36645c58f3a194767f3182bcbe23
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-02-13 20:32:14 -0300

    Fix github actions, lint project and settings.py databases

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('todo_api.urls'), name='task'),
-    path('api/user/', include('user_api.urls'), name='user'),
-    path("api/rest/auth/", include("dj_rest_auth.urls"), name='rest-auth'),  # endpoints provided by dj-rest-auth
-    path('api/auth/', include('auth_api.urls'), name='auth'),
+    path("admin/", admin.site.urls),
+    path("api/", include("todo_api.urls"), name="task"),
+    path("api/user/", include("user_api.urls"), name="user"),
+    path(
+        "api/rest/auth/", include("dj_rest_auth.urls"), name="rest-auth"
+    ),  # endpoints provided by dj-rest-auth
+    path("api/auth/", include("auth_api.urls"), name="auth"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
