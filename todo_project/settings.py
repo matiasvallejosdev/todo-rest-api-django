3964d6a2611cda20b7e816e98701bf3a30f5b34a
Matias Vallejos <matiasvallejosdev@outlook.com>
2024-02-13 19:14:31 -0300

    STATICFILES_DIRS Tuple fixed

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 MEDIA_URL = "images/"
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "images")
 
